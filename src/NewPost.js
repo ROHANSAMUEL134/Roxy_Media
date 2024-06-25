2099b1f572e0d176215d451b24d02805ba7f9e89
ROHAN SAMUEL J <rohansamuel134@gmail.com>
2024-06-25 15:34:26 +0530

    Update NewPost.js

--- src/NewPost.js
+++ src/NewPost.js
@@ -4,7 +4,7 @@ const NewPost = ({
 
     return (
         <main className="NewPost">
-            <h2 style={{color:'hsl(0, 100%, 70%)'}}>New Post</h2>
+            <h2>New Post</h2>
             <form className="newPostForm" onSubmit={handleSubmit}>
                 <label htmlFor="postTitle">Title:</label>
                 <input
